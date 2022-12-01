dddd2711d77cf6de9562049d1f08ef6b13341613
ppmdatix <peseupau@801-vgpu-scr.univ-rouen.fr>
2022-12-01 16:17:31 +0000

    on va voir

--- nn_experiments/commands401.sh
+++ nn_experiments/commands401.sh
@@ -1,19 +1,19 @@
 
-python mnist_launch.py --exp_name=0000-ssb-from-rad-argmean-K200-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --argmean=True --repeat_ssb=200   --draw_ssb=10 --supersub_from_rad=True
-python mnist_launch.py --exp_name=0001-ssb-from-rad-argmean-K200-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --argmean=True --repeat_ssb=200   --draw_ssb=10 --supersub_from_rad=True
-python mnist_launch.py --exp_name=0002-ssb-from-rad-argmean-K200-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --argmean=True --repeat_ssb=200   --draw_ssb=10 --supersub_from_rad=True
-python mnist_launch.py --exp_name=0003-ssb-from-rad-argmean-K200-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --argmean=True --repeat_ssb=200   --draw_ssb=10 --supersub_from_rad=True
-python mnist_launch.py --exp_name=0004-ssb-from-rad-argmean-K200-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --argmean=True --repeat_ssb=200   --draw_ssb=10 --supersub_from_rad=True
+python mnist_launch.py --exp_name=0000-ssb-from-rad-K200-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1   --repeat_ssb=200   --draw_ssb=10 --supersub_from_rad=True
+python mnist_launch.py --exp_name=0001-ssb-from-rad-K200-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1   --repeat_ssb=200   --draw_ssb=10 --supersub_from_rad=True
+python mnist_launch.py --exp_name=0002-ssb-from-rad-K200-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1   --repeat_ssb=200   --draw_ssb=10 --supersub_from_rad=True
+python mnist_launch.py --exp_name=0003-ssb-from-rad-K200-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1   --repeat_ssb=200   --draw_ssb=10 --supersub_from_rad=True
+python mnist_launch.py --exp_name=0004-ssb-from-rad-K200-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1   --repeat_ssb=200   --draw_ssb=10 --supersub_from_rad=True
 
-python mnist_launch.py --exp_name=0000-ssb-from-rad-argmean-K500-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --argmean=True --repeat_ssb=500   --draw_ssb=10 --supersub_from_rad=True
-python mnist_launch.py --exp_name=0001-ssb-from-rad-argmean-K500-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --argmean=True --repeat_ssb=500   --draw_ssb=10 --supersub_from_rad=True
-python mnist_launch.py --exp_name=0002-ssb-from-rad-argmean-K500-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --argmean=True --repeat_ssb=500   --draw_ssb=10 --supersub_from_rad=True
-python mnist_launch.py --exp_name=0003-ssb-from-rad-argmean-K500-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --argmean=True --repeat_ssb=500   --draw_ssb=10 --supersub_from_rad=True
-python mnist_launch.py --exp_name=0004-ssb-from-rad-argmean-K500-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --argmean=True --repeat_ssb=500   --draw_ssb=10 --supersub_from_rad=True
+python mnist_launch.py --exp_name=0000-ssb-from-rad-K500-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1   --repeat_ssb=500   --draw_ssb=10 --supersub_from_rad=True
+python mnist_launch.py --exp_name=0001-ssb-from-rad-K500-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1   --repeat_ssb=500   --draw_ssb=10 --supersub_from_rad=True
+python mnist_launch.py --exp_name=0002-ssb-from-rad-K500-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1   --repeat_ssb=500   --draw_ssb=10 --supersub_from_rad=True
+python mnist_launch.py --exp_name=0003-ssb-from-rad-K500-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1   --repeat_ssb=500   --draw_ssb=10 --supersub_from_rad=True
+python mnist_launch.py --exp_name=0004-ssb-from-rad-K500-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1   --repeat_ssb=500   --draw_ssb=10 --supersub_from_rad=True
 
 
-python mnist_launch.py --exp_name=0000-ssb-from-rad-argmean-K1000-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --argmean=True --repeat_ssb=1000   --draw_ssb=10 --supersub_from_rad=True
-python mnist_launch.py --exp_name=0001-ssb-from-rad-argmean-K1000-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --argmean=True --repeat_ssb=1000   --draw_ssb=10 --supersub_from_rad=True
-python mnist_launch.py --exp_name=0002-ssb-from-rad-argmean-K1000-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --argmean=True --repeat_ssb=1000   --draw_ssb=10 --supersub_from_rad=True
-python mnist_launch.py --exp_name=0003-ssb-from-rad-argmean-K1000-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --argmean=True --repeat_ssb=1000   --draw_ssb=10 --supersub_from_rad=True
-python mnist_launch.py --exp_name=0004-ssb-from-rad-argmean-K1000-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --argmean=True --repeat_ssb=1000   --draw_ssb=10 --supersub_from_rad=True
+python mnist_launch.py --exp_name=0000-ssb-from-rad-K1000-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --repeat_ssb=1000   --draw_ssb=10 --supersub_from_rad=True
+python mnist_launch.py --exp_name=0001-ssb-from-rad-K1000-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --repeat_ssb=1000   --draw_ssb=10 --supersub_from_rad=True
+python mnist_launch.py --exp_name=0002-ssb-from-rad-K1000-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --repeat_ssb=1000   --draw_ssb=10 --supersub_from_rad=True
+python mnist_launch.py --exp_name=0003-ssb-from-rad-K1000-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  --repeat_ssb=1000   --draw_ssb=10 --supersub_from_rad=True
+python mnist_launch.py --exp_name=0004-ssb-from-rad-K1000-10choice   --lr=0.000527 --weight_decay=1.009799e-03 --keep_frac=0.1  ---repeat_ssb=1000   --draw_ssb=10 --supersub_from_rad=True
