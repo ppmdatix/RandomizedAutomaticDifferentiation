823a7574a26c5754073264709cde1861d21cbaa3
ppmdatix <paul.peseux@gmail.com>
2023-03-20 11:50:29 +0100

    ready for more

--- nn_experiments/cifar.sh
+++ nn_experiments/cifar.sh
@@ -1,34 +1,44 @@
-python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-baseline --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=1.0  --supersub=False
-python cifar_launch.py  --exp_root=cifarexperiments --exp_name=03-baseline --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=1.0  --supersub=False
-
-
-# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=00-ssb-Kmax100-M5-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=100   --draw_ssb=5 --supersub=True
-python cifar_launch.py  --exp_root=cifarexperiments --exp_name=01-ssb-Kmax100-M5-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=100   --draw_ssb=5 --supersub=True
-python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-ssb-Kmax100-M5-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=100   --draw_ssb=5 --supersub=True
-
-
-# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=00-ssb-Kmax10-M10-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=10   --draw_ssb=10 --supersub=True
-python cifar_launch.py  --exp_root=cifarexperiments --exp_name=01-ssb-Kmax10-M10-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=10   --draw_ssb=10 --supersub=True
-python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-ssb-Kmax10-M10-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=10   --draw_ssb=10 --supersub=True
-
-# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=00-ssb-Kmax100-M1-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=100   --draw_ssb=1 --supersub=True
-python cifar_launch.py  --exp_root=cifarexperiments --exp_name=01-ssb-Kmax100-M1-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=100   --draw_ssb=1 --supersub=True
-python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-ssb-Kmax100-M1-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=100   --draw_ssb=1 --supersub=True
-
-
-# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=00-ssb-Kmax1-M10-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=1   --draw_ssb=10 --supersub_from_rad=True
-python cifar_launch.py  --exp_root=cifarexperiments --exp_name=01-ssb-Kmax1-M10-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=1   --draw_ssb=10 --supersub=True
-python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-ssb-Kmax1-M10-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=1   --draw_ssb=10 --supersub=True
-
-
-
-python cifar_launch.py  --exp_root=cifarexperiments --exp_name=01-ssb-Kmax1-M5-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=1   --draw_ssb=5 --supersub=True
-python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-ssb-Kmax1-M5-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=1   --draw_ssb=5 --supersub=True
-
-
-python cifar_launch.py  --exp_root=cifarexperiments --exp_name=01-ssb-Kmax5-M1-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=5   --draw_ssb=1 --supersub=True
+# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-baseline --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=1.0  --supersub=False
+# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=03-baseline --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=1.0  --supersub=False
+#
+#
+# # python cifar_launch.py  --exp_root=cifarexperiments --exp_name=00-ssb-Kmax100-M5-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=100   --draw_ssb=5 --supersub=True
+# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=01-ssb-Kmax100-M5-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=100   --draw_ssb=5 --supersub=True
+# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-ssb-Kmax100-M5-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=100   --draw_ssb=5 --supersub=True
+#
+#
+# # python cifar_launch.py  --exp_root=cifarexperiments --exp_name=00-ssb-Kmax10-M10-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=10   --draw_ssb=10 --supersub=True
+# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=01-ssb-Kmax10-M10-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=10   --draw_ssb=10 --supersub=True
+# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-ssb-Kmax10-M10-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=10   --draw_ssb=10 --supersub=True
+#
+# # python cifar_launch.py  --exp_root=cifarexperiments --exp_name=00-ssb-Kmax100-M1-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=100   --draw_ssb=1 --supersub=True
+# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=01-ssb-Kmax100-M1-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=100   --draw_ssb=1 --supersub=True
+# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-ssb-Kmax100-M1-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=100   --draw_ssb=1 --supersub=True
+#
+#
+# # python cifar_launch.py  --exp_root=cifarexperiments --exp_name=00-ssb-Kmax1-M10-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=1   --draw_ssb=10 --supersub_from_rad=True
+# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=01-ssb-Kmax1-M10-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=1   --draw_ssb=10 --supersub=True
+# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-ssb-Kmax1-M10-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=1   --draw_ssb=10 --supersub=True
+#
+#
+#
+# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=01-ssb-Kmax1-M5-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=1   --draw_ssb=5 --supersub=True
+# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-ssb-Kmax1-M5-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=1   --draw_ssb=5 --supersub=True
+#
+#
+# python cifar_launch.py  --exp_root=cifarexperiments --exp_name=01-ssb-Kmax5-M1-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=5   --draw_ssb=1 --supersub=True
 python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-ssb-Kmax5-M1-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=5   --draw_ssb=1 --supersub=True
 
 
 python cifar_launch.py  --exp_root=cifarexperiments --exp_name=01-ssb-Kmax5-M5-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=5   --draw_ssb=5 --supersub=True
 python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-ssb-Kmax5-M5-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=5   --draw_ssb=5 --supersub=True
+
+
+python cifar_launch.py  --exp_root=cifarexperiments --exp_name=00-ssb-big_batch300-Kmax10-M10-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=10 --draw_ssb=10 --supersub=True --batch_size=300
+python cifar_launch.py  --exp_root=cifarexperiments --exp_name=01-ssb-big_batch300-Kmax10-M10-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=10 --draw_ssb=10 --supersub=True --batch_size=300
+python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-ssb-big_batch300-Kmax10-M10-0o1kf --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1   --repeat_ssb=10 --draw_ssb=10 --supersub=True --batch_size=300
+
+
+python cifar_launch.py  --exp_root=cifarexperiments --exp_name=00-dropout --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1  --supersub=False --dropout=True
+python cifar_launch.py  --exp_root=cifarexperiments --exp_name=01-dropout --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1  --supersub=False --dropout=True
+python cifar_launch.py  --exp_root=cifarexperiments --exp_name=02-dropout --simple=True --lr=0.000435 --weight_decay=5.485464e-05 --keep_frac=0.1  --supersub=False --dropout=True
